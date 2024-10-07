f11ac78ca437cfb06f268fe6d7bb46e1765f6490
HeatherKates <hkates@ufl.edu>
2024-10-07 10:20:14 -0400

    update var names

--- ATACseq/MLH1/scripts/7_diffbind.R
+++ ATACseq/MLH1/scripts/7_diffbind.R
@@ -1,21 +1,22 @@
 library(DiffBind)
 library(edgeR)
 library(BiocParallel)
+library(dplyr)
 
 # Set up parallelization to use 4 cores
 bp_param <- MulticoreParam(workers = 6)
 
 samples <- data.frame(
-  SampleID = c("MSH2KO-1", "MSH2KO-2", "MSH2KO-3", "MSH2R4-1", "MSH2R4-2", "MSH2R4-3"),
-  Tissue = c(rep("KO", 3), rep("MSH2", 3)),  #  'Tissue' can represent conditions or leave this column out.
-  Factor = c(rep("MSH2KO", 3), rep("MSH2R4", 3)),
-  Condition = c(rep("MSH2KO", 3), rep("MSH2", 3)),  # The experimental conditions
+  SampleID = c("MLH1KO-1", "MLH1KO-2", "MLH1KO-3", "MLH1R4-1", "MLH1R4-2", "MLH1R4-3"),
+  Tissue = c(rep("KO", 3), rep("MLH1", 3)),  #  'Tissue' can represent conditions or leave this column out.
+  Factor = c(rep("MLH1KO", 3), rep("MLH1R4", 3)),
+  Condition = c(rep("MLH1KO", 3), rep("MLH1", 3)),  # The experimental conditions
   Treatment = rep("None", 6),  # Since there's no treatment in this setup
   Replicate = c(1, 2, 3, 1, 2, 3),  # Indicating replicates
-  bamReads = c("../6_bigwig/MSH2KO-1_sort_n.bam", "../6_bigwig/MSH2KO-2_sort_n.bam", "../6_bigwig/MSH2KO-3_sort_n.bam",
-               "../6_bigwig/MSH2R4-1_sort_n.bam", "../6_bigwig/MSH2R4-2_sort_n.bam", "../6_bigwig/MSH2R4-3_sort_n.bam"),
-  Peaks = c("../5_genrich/MSH2KO-1.narrowPeak", "../5_genrich/MSH2KO-2.narrowPeak", "../5_genrich/MSH2KO-3.narrowPeak",
-            "../5_genrich/MSH2R4-1.narrowPeak", "../5_genrich/MSH2R4-2.narrowPeak", "../5_genrich/MSH2R4-3.narrowPeak"),
+  bamReads = c("../6_bigwig/MLH1KO-1_sort_n.bam", "../6_bigwig/MLH1KO-2_sort_n.bam", "../6_bigwig/MLH1KO-3_sort_n.bam",
+               "../6_bigwig/MLH1R4-1_sort_n.bam", "../6_bigwig/MLH1R4-2_sort_n.bam", "../6_bigwig/MLH1R4-3_sort_n.bam"),
+  Peaks = c("../5_genrich/MLH1KO-1.narrowPeak", "../5_genrich/MLH1KO-2.narrowPeak", "../5_genrich/MLH1KO-3.narrowPeak",
+            "../5_genrich/MLH1R4-1.narrowPeak", "../5_genrich/MLH1R4-2.narrowPeak", "../5_genrich/MLH1R4-3.narrowPeak"),
   PeakCaller = rep("narrow", 6),  # For Genrich, 'narrow' for narrowPeak format
   stringsAsFactors = FALSE
 )
@@ -34,7 +35,8 @@ dbaObj <- dba.contrast(dbaObj, categories = DBA_CONDITION)
 
 # Perform differential binding analysis
 dbaObj <- dba.analyze(dbaObj)
-saveRDS(dbaObj,file="../7_diffbind/MSH2_dbaObj.RDATA")
+saveRDS(dbaObj,file="../7_diffbind/MLH1_dbaObj.RDATA")
+
 # Generate and inspect the report of differentially bound regions
 diffPeaks <- dba.report(dbaObj)
 head(diffPeaks)
@@ -66,13 +68,13 @@ write_peaks_to_bed <- function(peaks_df, output_file) {
 }
 
 # Example usage with your dataframe 'diffPeaksIn'
-write_peaks_to_bed(diffPeaksIn, "../7_diffbind/differential_binding_results.bed")
+write_peaks_to_bed(diffPeaksIn, "../7_diffbind/MLH1_differential_binding_results.bed")
 
 #Make a README
 # Define column names and their descriptions
 column_descriptions <- data.frame(
   Heading = c("X", "seqnames", "start", "end", "width", "strand", "Conc", 
-              "Conc_MSH2", "Conc_KO", "Fold", "p.value", "FDR"),
+              "Conc_MLH1", "Conc_KO", "Fold", "p.value", "FDR"),
   Description = c(
     "Row index or identifier (not relevant for analysis)",
     "Chromosome or contig name where the peak is located",
@@ -81,10 +83,10 @@ column_descriptions <- data.frame(
     "Width of the peak (end - start)",
     "Strand information (+/-) where the peak is located, if applicable",
     "Average read concentration (normalized counts) across all samples",
-    "Average read concentration in MSH2 samples (wild-type or tagged)",
+    "Average read concentration in MLH1 samples (wild-type or tagged)",
     "Average read concentration in KO (knockout) samples",
-    "Log2 fold change of read concentration between KO and MSH2 samples",
-    "Raw p-value for differential binding between KO and MSH2 samples",
+    "Log2 fold change of read concentration between KO and MLH1 samples",
+    "Raw p-value for differential binding between KO and MLH1 samples",
     "False discovery rate (adjusted p-value) for differential binding"
   ),
   stringsAsFactors = FALSE
@@ -94,18 +96,18 @@ column_descriptions <- data.frame(
 print(column_descriptions)
 
 # Set thresholds
-min_Conc_MSH2 <- 2  # Adjust this value based on data and expectations
-max_Conc_KO <- 0.2  # Adjust based on definition of "low" in KO
+min_Conc_MLH1 <- 2  # Adjust this value based on data and expectations
+max_Conc_MLH1KO <- 0.2  # Adjust based on definition of "low" in KO
 
-# Filter peaks for positive fold change, low concentration in KO, and reasonably high concentration in MSH2
+# Filter peaks for positive fold change, low concentration in KO, and reasonably high concentration in MLH1
 filtered_peaks <- diffPeaksIn %>%
-  filter(Fold > 0 & Conc_KO < max_Conc_KO & Conc_MSH2 > min_Conc_MSH2)
+  filter(Fold > 0 & Conc_MLH1KO < max_Conc_MLH1KO & Conc_MLH1 > min_Conc_MLH1)
 
 # Check the filtered results
 head(filtered_peaks)
-write_peaks_to_bed(filtered_peaks, "../7_diffbind/filtered_differential_binding_results.bed")
+write_peaks_to_bed(filtered_peaks, "../7_diffbind/MLH1_filtered_differential_binding_results.bed")
 
 #From this point, you can view filtered peaks and per-sample reads in IGV by loading:
 #"../7_diffbind/filtered_differential_binding_results.bed"
-#"../6_bigwig/MSH2KO-1.bw","../6_bigwig/MSH2KO-2.bw","../6_bigwig/MSH2KO-3.bw","../6_bigwig/MSH2R4-1.bw","../6_bigwig/MSH2R4-2.bw","../6_bigwig/MSH2R4-3.bw")
+#"../6_bigwig/MLH1KO-1.bw","../6_bigwig/MLH1KO-2.bw","../6_bigwig/MLH1KO-3.bw","../6_bigwig/MLH1R4-1.bw","../6_bigwig/MLH1R4-2.bw","../6_bigwig/MLH1R4-3.bw")
 
